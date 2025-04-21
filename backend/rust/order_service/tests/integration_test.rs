e0b8b9d5294b3d67084015d2954bb895d2bc90b8
alkuzin <alkuzindev@gmail.com>
2025-04-21 18:10:58 +0300

    test: added test for create_order request handler

---
+++ backend/rust/order_service/tests/integration_test.rs
@@ -0,0 +1,92 @@
+// Skate - courier delivery service.
+// Copyright (C) 2025 Alexander (@alkuzin).
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//! Order service integration tests main module.
+
+use order_service::{create_order, service::OrderService, config};
+use actix_web::{test, web, App};
+use actix_web::http::StatusCode;
+use serde_json::json;
+
+async fn setup_order_service() -> OrderService {
+    let mut service = OrderService::new(config::TEST_DATABASE_PATH).await
+        .expect("OrderService::new() should succeed!");
+
+    let _ = service.init().await
+        .expect("OrderService::init() should succeed!");
+
+    service
+}
+
+#[actix_web::test]
+async fn test_setup_order_service() {
+    let _ = setup_order_service();
+}
+
+#[actix_web::test]
+async fn test_create_order() {
+    // Create a test app with the service.
+    let service = setup_order_service().await;
+
+    let app = test::init_service(
+        App::new()
+            .app_data(web::Data::new(service))
+            .route("/orders", web::post().to(create_order))
+    ).await;
+
+    let order = json!({
+        "dto": {
+            "order_id":     1,
+            "customer_id":  123,
+            "order_status": "Completed",
+            "address":      "123 Main St, Anytown, USA",
+            "price":        1000
+        },
+        "items": [
+            {
+                "order_id":    0,
+                "product_id":  456,
+                "quantity":    2,
+                "unit_price":  500,
+                "total_price": 1000
+            }
+        ]
+    });
+
+    // Send a POST request to the /orders endpoint.
+    let req = test::TestRequest::post()
+        .uri("/orders")
+        .set_json(&order)
+        .to_request();
+
+    // Call a service.
+    let response = test::call_service(&app, req).await;
+    let status   = response.status();
+
+    // Print the response body.
+    let body_bytes  = test::read_body(response).await;
+    let body_string = String::from_utf8_lossy(&body_bytes);
+
+    println!("Received response: {}", body_string);
+    assert_eq!(status, StatusCode::CREATED);
+
+    if let Ok(id) = body_string.trim().parse::<i64>() {
+        println!("Created order with ID: {}", id);
+    }
+    else {
+        eprintln!("Error to parse order ID");
+    }
+}
