c69a553390bbc6eaed5c2dd0cab46ccb32a30f5a
alkuzin <alkuzindev@gmail.com>
2025-04-27 21:57:35 +0300

    feat: added product microservice

---
+++ backend/rust/product_service/src/main.rs
@@ -0,0 +1,40 @@
+// Skate - courier delivery service.
+// Copyright (C) 2025 Alexander (@alkuzin).
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//! Product service entry point.
+
+use product_service::{config, service::ProductService};
+use actix_web::{web::Data, App, HttpServer};
+use std::error::Error;
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("[product-service] Running server");
+    println!("[product-service] Connecting database");
+
+    let service = ProductService::new(config::DATABASE_PATH).await?;
+
+    // Run HTTP server.
+    let _ = HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(service.clone()))
+    })
+        .bind(config::BIND_ADDRESS)?
+        .run()
+        .await;
+
+    Ok(())
+}
