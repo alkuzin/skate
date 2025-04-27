e345fb618cb12e8ca545feb1ef0cf0b7e2b5b6cb
alkuzin <alkuzindev@gmail.com>
2025-04-27 21:56:18 +0300

    feat: added product service config module

---
+++ backend/rust/product_service/src/config.rs
@@ -0,0 +1,26 @@
+// Skate - courier delivery service.
+// Copyright (C) 2025 Alexander (@alkuzin).
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//! Service configuration data.
+
+/// Service bind address.
+pub const BIND_ADDRESS: &str = "127.0.0.1:4000";
+
+/// Service SQLite database path.
+pub const DATABASE_PATH: &str = "../db/products.db";
+
+/// Service SQLite database path for unit tests.
+pub const TEST_DATABASE_PATH: &str = "../db/test_products.db";
