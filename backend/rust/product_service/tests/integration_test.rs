be1edaf5ade8d5f44b131d78060581312243058d
alkuzin <alkuzindev@gmail.com>
2025-04-28 20:11:14 +0300

    test: added test for getting product

--- backend/rust/product_service/tests/integration_test.rs
+++ backend/rust/product_service/tests/integration_test.rs
@@ -16,9 +16,10 @@
 
 //! Product service integration tests main module.
 
-use product_service::{service::ProductService, config, create_product, create_category};
+use product_service::{service::ProductService, config, create_product, create_category, get_product};
 use actix_web::{test, web, App, http::StatusCode};
 use serde_json::json;
+use product_service::product::Product;
 
 async fn setup_product_service() -> ProductService {
     let service = ProductService::new(config::TEST_DATABASE_PATH).await
@@ -117,5 +118,45 @@ async fn test_create_category() {
     }
     else {
         eprintln!("Error to parse category ID");
+    }
+}
+
+#[actix_web::test]
+async fn test_get_product() {
+    // Create a test app with the service.
+    let service = setup_product_service().await;
+
+    let app = test::init_service(
+        App::new()
+            .app_data(web::Data::new(service))
+            .route("/products/{id}", web::get().to(get_product))
+    ).await;
+
+    let product_id = json!(1);
+
+    // Send a GET request to the /products/{id} endpoint.
+    let req = test::TestRequest::get()
+        .uri(format!("/products/{}", product_id).as_str())
+        .set_json(&product_id)
+        .to_request();
+
+    // Call a service.
+    let response = test::call_service(&app, req).await;
+    let status   = response.status();
+
+    // Print the response body.
+    let body_bytes  = test::read_body(response).await;
+    let body_string = String::from_utf8_lossy(&body_bytes);
+
+    println!("Received response: {}", body_string);
+    assert_eq!(status, StatusCode::CREATED, "DB is empty");
+
+    if let Ok(product) = serde_json::from_str::<Product>(
+        body_string.into_owned().as_str()
+    ) {
+        println!("Get product: {:#?}", product);
+    }
+    else {
+        eprintln!("Error to parse product");
     }
 }
