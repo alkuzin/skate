7d6d327ac47b1135c11cd0343723a902e1ebdfda
alkuzin <alkuzindev@gmail.com>
2025-04-28 20:22:48 +0300

    test: added test for getting category

--- backend/rust/product_service/tests/integration_test.rs
+++ backend/rust/product_service/tests/integration_test.rs
@@ -16,10 +16,10 @@
 
 //! Product service integration tests main module.
 
-use product_service::{service::ProductService, config, create_product, create_category, get_product};
+use product_service::{service::ProductService, config, create_product, create_category, get_product, get_category};
 use actix_web::{test, web, App, http::StatusCode};
 use serde_json::json;
-use product_service::product::Product;
+use product_service::product::{Category, Product};
 
 async fn setup_product_service() -> ProductService {
     let service = ProductService::new(config::TEST_DATABASE_PATH).await
@@ -158,5 +158,45 @@ async fn test_get_product() {
     }
     else {
         eprintln!("Error to parse product");
+    }
+}
+
+#[actix_web::test]
+async fn test_get_category() {
+    // Create a test app with the service.
+    let service = setup_product_service().await;
+
+    let app = test::init_service(
+        App::new()
+            .app_data(web::Data::new(service))
+            .route("/categories/{id}", web::get().to(get_category))
+    ).await;
+
+    let category_id = json!(1);
+
+    // Send a GET request to the /category/{id} endpoint.
+    let req = test::TestRequest::get()
+        .uri(format!("/categories/{}", category_id).as_str())
+        .set_json(&category_id)
+        .to_request();
+
+    // Call a service.
+    let response = test::call_service(&app, req).await;
+    let status   = response.status();
+
+    // Print the response body.
+    let body_bytes  = test::read_body(response).await;
+    let body_string = String::from_utf8_lossy(&body_bytes);
+
+    println!("Received response: {}", body_string);
+    assert_eq!(status, StatusCode::CREATED, "DB is empty");
+
+    if let Ok(category) = serde_json::from_str::<Category>(
+        body_string.into_owned().as_str()
+    ) {
+        println!("Get product category: {:#?}", category);
+    }
+    else {
+        eprintln!("Error to parse product category");
     }
 }
