6bbcbedca50270838610f21a6e1ad7ae8e5ddebc
Mansur <mansurgumeroa@gmail.com>
2025-06-03 05:35:29 +0300

    )

--- backend/golang/internal/models/customer.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Customer struct {
-	UserID  int    `json:"user_id" db:"user_id"`
-	Name    string `json:"name" db:"name"`
-	Address string `json:"address" db:"address"`
-	Phone   string `json:"phone" db:"phone"`
-}
